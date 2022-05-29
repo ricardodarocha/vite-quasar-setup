67e468460e72d093b6275f6870aea7f9dcf7a4b1
Ricardo da Rocha <ricardodarocha@outlook.com>
2022-05-29 13:28:57 -0300

    initial

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/pt-BR'
+import quasarIconSet from 'quasar/icon-set/svg-material-icons'
+
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {}, 
+    lang: quasarLang,
+    iconSet: quasarIconSet,
+})
+
+myApp.mount('#app')
